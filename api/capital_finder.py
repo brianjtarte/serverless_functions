70e6bd3f804f2e6894de4d84e7d17e3668070295
brianjtarte <brianjtarte@gmail.com>
2022-07-13 20:35:24 -0700

    testing get request

---
+++ api/capital_finder.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def get_country(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        r = requests.get('https://restcountries.com/v3.1/name/peru')
+        print(r.text)
+        return
