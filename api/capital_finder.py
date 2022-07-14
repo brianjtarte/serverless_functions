245cd4bc66d7260c6d2463292e15136d0fee6141
brianjtarte <brianjtarte@gmail.com>
2022-07-13 21:12:41 -0700

    testing again

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -10,15 +10,16 @@ class handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(url_path)
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
-
-        url = 'https://restcountries.com/v3.1/all'
-        r = requests.get(url + dic['country'])
-
-        info = r.json()
-        message = f"The country is {info['country']}"
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-
-        self.wfile.write(message.encode())
+        print(dic)
+        # url = 'https://restcountries.com/v3.1/all'
+        #
+        # r = requests.get(url + dic['country'])
+        #
+        # info = r.json()
+        # message = f"The country is {info['country']}"
+        # self.send_response(200)
+        # self.send_header('Content-type', 'text/plain')
+        # self.end_headers()
+        #
+        # self.wfile.write(message.encode())
         return
